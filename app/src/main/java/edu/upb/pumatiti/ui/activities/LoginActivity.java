3afcdd15ef896a815f151f777dcec0ce797eecfc
andresvasquezagramont <andres.vasquez.agramont@gmail.com>
2019-11-21 09:11:58 -0400

    Base UI structure.

---
+++ app/src/main/java/edu/upb/pumatiti/ui/activities/LoginActivity.java
@@ -0,0 +1,72 @@
+package edu.upb.pumatiti.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import edu.upb.pumatiti.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String LOG = LoginActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.e(LOG, "onCreate");
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("message")) {
+            String message = intent.getStringExtra("message");
+            Toast.makeText(this, // Context, origen
+                    message, //Mensaje
+                    Toast.LENGTH_SHORT) //Duración
+                    .show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(LOG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(LOG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(LOG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(LOG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(LOG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(LOG, "onDestroy");
+    }
+
+    public void registerClick(View view) {
+        Log.e(LOG, "registerClick");
+    }
+}
