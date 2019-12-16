8be7fe15cd81f5b228925006a952cbd8b0b3527b
andresvasquezagramont <andres.vasquez.agramont@gmail.com>
2019-12-16 08:27:56 -0400

    Firebase --> Base.

--- app/src/main/java/edu/upb/pumatiti/repository/firebase/FirebaseRepository.java
+++ app/src/main/java/edu/upb/pumatiti/repository/firebase/FirebaseRepository.java
@@ -1,5 +1,7 @@
 package edu.upb.pumatiti.repository.firebase;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -10,12 +12,26 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import edu.upb.pumatiti.models.repository.Base;
+import edu.upb.pumatiti.models.repository.Bus;
+import edu.upb.pumatiti.models.repository.Student;
 
 public class FirebaseRepository {
     private static FirebaseRepository instance;
     private FirebaseAuth auth;
+    private FirebaseDatabase db;
 
     public static FirebaseRepository getInstance() {
         if (instance == null) {
@@ -26,6 +42,7 @@ public class FirebaseRepository {
 
     private FirebaseRepository() {
         auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
     }
 
     public LiveData<Base> login(final String email, final String password) {
@@ -37,6 +54,11 @@ public class FirebaseRepository {
                         if (task.isSuccessful()) {
                             FirebaseUser user = auth.getCurrentUser();
                             results.postValue(new Base(user));
+
+
+                            subscribeToValues("users");
+                            Student student = new Student("Andrés", 29);
+                            setValue("users/99999", student);
                         } else {
                             results.postValue(new Base("login Failure",
                                     new NullPointerException()));
@@ -57,4 +79,28 @@ public class FirebaseRepository {
         this.auth.createUserWithEmailAndPassword(email, password);
         return results;
     }
+
+    public LiveData<Base> setValue(String path, Object value) {
+        final MutableLiveData<Base> result = new MutableLiveData<>();
+        db.getReference(path).setValue(value);
+        return result;
+    }
+
+    public LiveData<Base> subscribeToValues(String path) {
+        final MutableLiveData<Base> result = new MutableLiveData<>();
+        db.getReference(path).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String values = new Gson().toJson(dataSnapshot.getValue());
+                Log.e("Database", values);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return result;
+    }
+
 }
